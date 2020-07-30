a0f87acc22ee618eb1aacdeae120ddc64895c5e3
Yaz <kaplan3260@gmail.com>
2020-07-30 00:55:41 -0500

    added CS-6

---
+++ CS-6/Coding Challenge - 006.py
@@ -0,0 +1,23 @@
+#Most Water
+def maxW(arr,n) : 
+    arr=list(map(int,arr)) 
+    res = 0;  
+    for i in range(1, n - 1) :    
+        left = arr[i];  
+        for j in range(i) : 
+            left = max(left, arr[j]);  
+        right = arr[i];  
+        for j in range(i + 1 , n) :  
+            right = max(right, arr[j]); 
+        res=res+(min(left, right) - arr[i]);
+    return res;  
+
+resList=[]
+while True:
+    response=input("Type 'ok' when you are done: ")
+    resList.append(response)
+    if response.lower()=="ok": 
+        resList.pop()  
+        result=maxW(resList,len(resList))
+        print(result)
+        break
